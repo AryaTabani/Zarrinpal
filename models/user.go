ef1be3ef92806984e6f3c170ff6a80ea823bbf3d
Arya Tabani <a.tabani82@gmail.com>
2025-10-24 23:07:51 +0330

    login payload

--- models/user.go
+++ models/user.go
@@ -14,3 +14,8 @@ type RegisterPayload struct {
 	Email     string `json:"email"`
 	Password  string `json:"password"`
 }
+
+type LoginPayload struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
