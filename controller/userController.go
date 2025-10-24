e5b8ae7473fafff8284f90126dbb8c48bbb5434d
Arya Tabani <a.tabani82@gmail.com>
2025-10-24 23:09:20 +0330

    adding login handler

--- controller/userController.go
+++ controller/userController.go
@@ -45,3 +45,41 @@ func UserRegisterHandler() gin.HandlerFunc {
 		c.JSON(http.StatusOK, response)
 	}
 }
+
+func LoginHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var payload models.LoginPayload
+		err := c.ShouldBindBodyWithJSON(payload)
+		if err != nil {
+			response := models.APIResponse[any]{
+				Success: false,
+				Error:   "invalid request body",
+			}
+			c.JSON(http.StatusBadRequest, response)
+			return
+		}
+		token, err := services.LoginUser(c.Request.Context(), &payload)
+		if err != nil {
+			if errors.Is(err, services.ErrInvalidCredentials) {
+				response := models.APIResponse[any]{
+					Success: false,
+					Error:   err.Error(),
+				}
+				c.JSON(http.StatusUnauthorized, response)
+				return
+			}
+			response := models.APIResponse[any]{
+				Success: false,
+				Error:   "Login Failed",
+			}
+			c.JSON(http.StatusInternalServerError, response)
+			return
+		}
+		response := models.APIResponse[models.LoginResponse]{
+			Success: true,
+			Data:    models.LoginResponse{Token: token},
+		}
+		c.JSON(http.StatusOK, response)
+
+	}
+}
