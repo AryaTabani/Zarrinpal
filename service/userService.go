e67e9b1504424a379ac05c86cfefb3c0ebda40d2
Arya Tabani <a.tabani82@gmail.com>
2025-10-24 23:08:07 +0330

    adding login service

--- service/userService.go
+++ service/userService.go
@@ -5,17 +5,21 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"os"
+	"time"
 
 	"zarrinpal/models"
 	"zarrinpal/repository"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
-//var jwtSecret = []byte(os.Getenv("JWT_SECRET_KEY"))
+var jwtSecret = []byte(os.Getenv("JWT_SECRET_KEY"))
 
 var (
-	ErrUserExists = errors.New("a user with this email already exists")
+	ErrUserExists         = errors.New("a user with this email already exists")
+	ErrInvalidCredentials = errors.New("invalid email or password")
 )
 
 func RegisterUser(ctx context.Context, payload *models.RegisterPayload) (*models.User, error) {
@@ -43,4 +47,34 @@ func RegisterUser(ctx context.Context, payload *models.RegisterPayload) (*models
 		return nil, fmt.Errorf("could not create user: %w", err)
 	}
 	return newUser, nil
+}
+func LoginUser(ctx context.Context, payload *models.LoginPayload) (string, error) {
+	user, err := repository.GetUserByEmail(ctx, payload.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", ErrInvalidCredentials
+		}
+		return "", err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password_hash), []byte(payload.Password))
+	if err != nil {
+		return "", ErrInvalidCredentials
+	}
+	return generateUserToken(user.ID)
+}
+func generateUserToken(userID int64) (string, error) {
+	claims := jwt.MapClaims{
+		"sub": userID,
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", fmt.Errorf("could not sign token: %w", err)
+	}
+
+	return tokenString, nil
 }
