04b62f33cd003ad0bde90b650d4609e786317081
Arya Tabani <a.tabani82@gmail.com>
2025-10-25 12:27:33 +0330

    adding payment history

--- repository/userRepository.go
+++ repository/userRepository.go
@@ -24,3 +24,22 @@ func GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
 	)
 	return &user, err
 }
+
+func GetPaymentsHistory(ctx context.Context, userId int) ([]models.Payment, error) {
+	query := `SELECT id, amount, description, status FROM payments WHERE user_id = ?`
+	rows, err := db.DB.QueryContext(ctx, query, userId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var payments []models.Payment
+
+	for rows.Next() {
+		var p models.Payment
+		if err := rows.Scan(&p.ID, &p.Amount, &p.Description, &p.Status); err != nil {
+			return nil, err
+		}
+		payments = append(payments, p)
+	}
+	return payments, nil
+}
